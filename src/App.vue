84ad86f4874796c8a033a88dc7744a7e76c385fe
Teeradet <tee2490@hotmail.com>
2025-09-08 13:50:51 +0700

    13 axios in action

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,25 @@
-<template>Hello world</template>
+<template>
+  <div>RequestOPedia</div>
+  <hr />
+  <div v-for="user in userObj.users" :key="user.id">
+    <p>{{ user.name }}</p>
+    <p>{{ user.email }}</p>
+    <hr />
+  </div>
+</template>
+<script setup>
+import axios from "axios";
+import { reactive, onMounted } from "vue";
+
+const userObj = reactive({
+  users: [],
+});
+
+onMounted(() => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+    console.log(response.data);
+
+    userObj.users = response.data;
+  });
+});
+</script>
